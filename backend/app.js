8c40c33c69f0ac05516a9080380780d62ca41d2b
chidiu98 <34973870+koji98@users.noreply.github.com>
2019-04-30 18:39:34 -0400

    backend setup 2

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const pt = require('periodic-table');
 
 app.get('/', (req, res) => {
-  res.render('home', {data: pt.all()});
+  res.json({data: pt.all()});
 });
 
 app.listen(8000, () => console.log("Listening on port 8000"));
